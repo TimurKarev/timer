81c1878441d846b51fc261fd4fcbc263596ed520
Nik11978 <lilu_90@list.ru>
2022-01-24 21:21:17 +0300

    24janvar

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,90 @@
-#include <Arduino.h>
-#include <TimerMs.h>
-#include "lightpin.h"
-#include "temphumwire.h"
-#include "humiditer.h"
-
-#define DHT_PIN 2
-#define DHT_TYPE DHT11
-
-#define LIGHT1PIN 4
-
-#define DHT_TICK_PERIOD 5000
-
-#define LIGHT1_ON_PERIOD 72000000  // 20 часов
-#define LIGHT1_OFF_PERIOD 14400000 // 4 часа
-#define LIGHT1_START_PERIOD 18000000
-
-DHT Dht(DHT_PIN, DHT_TYPE);
-Humiditer hum = Humiditer(&Dht);
-
-bool isStartDelayPass = false;
-int time = 0;
-
-// (период, мс), (0 не запущен / 1 запущен), (режим: 0 период / 1 таймер)
-TimerMs strDelTimer(5000, 1, 1);
-
-// Установка таймера освещения
-LightPin lightPin(4, true, LIGHT1_START_PERIOD, LIGHT1_OFF_PERIOD, LIGHT1_ON_PERIOD);
-TempHumWire tempHumWire = TempHumWire(&Dht);
-
-void start_delay_pass()
-{
-  isStartDelayPass = true;
-  Serial.println("Start delay callback");
-}
-
-void setup()
-{
+#include "DHT.h"
+#define DHTPIN 2 // Тот самый номер пина, о котором упоминалось выше
+// Одна из следующих строк закоментирована. Снимите комментарий, если подключаете датчик DHT11 к arduino
+//DHT dht(DHTPIN, DHT22); //Инициация датчика
+DHT dht(DHTPIN, DHT11);
+#define PIN_Heating 5
+#define PIN_Fan 6
+#define PIN_Vaporizer_power 7
+#define PIN_Vaporizer_on 8
+boolean flag = true;
+const int ledPin = 4;
+int State = LOW;
+unsigned long previousMillis;
+const long interval_1 = 64800000; //Выкл
+const long interval_2 = 21600000; //Вкл
+void setup() {
   Serial.begin(9600);
-  Dht.begin();
-
-  strDelTimer.setTimerMode();
-  void (*start_delay_pass_ptr)() = &start_delay_pass;
-  strDelTimer.attach(start_delay_pass_ptr);
+  dht.begin();
+  pinMode(PIN_Heating, OUTPUT); // Объявляем пин реле как выход
+  pinMode(PIN_Fan, OUTPUT);
+  pinMode(PIN_Vaporizer_power, OUTPUT);
+  pinMode(PIN_Vaporizer_on, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 }
-
-void loop()
-{
-  if (!isStartDelayPass)
-  {
-    strDelTimer.tick();
+void loop() {
+  delay(2000); // 2 секунды задержки
+  float h = dht.readHumidity(); //Измеряем влажность
+  float t = dht.readTemperature(); //Измеряем температуру
+  if (isnan(h) || isnan(t)) {  // Проверка. Если не удается считать показания, выводится «Ошибка считывания», и программа завершает работу
+    Serial.println("Ошибка считывания");
     return;
   }
-  lightPin.tick();
-  tempHumWire.tick();
-  hum.tick();
+  {
+    float h = dht.readHumidity(); //Измеряем влажность
+    float t = dht.readTemperature(); //Измеряем температуру
+    //Обогреватель
+    if ((t) < 25) {
+      digitalWrite(PIN_Heating, HIGH);
+    }
+    if ((t) > 27) {
+      digitalWrite(PIN_Heating, LOW);
+    }
+    //Вытяжка
+    if ((t) > 29 || (h) > 95) {
+      digitalWrite(PIN_Fan, HIGH);
+    }
+    if ((t) < 29 & (h) < 90) {
+      digitalWrite(PIN_Fan, LOW);
+    }
+    //Испаритель питание
+    if ((h) < 75) {
+      digitalWrite(PIN_Vaporizer_power, HIGH);
+    }
+    if ((h) > 80) {
+      digitalWrite(PIN_Vaporizer_power, LOW);
+      delay(500);
+    }
+  }
+  //Испаритель управление
+  if (digitalRead(PIN_Vaporizer_power) && flag) {
+    flag = !flag;
+    digitalWrite(PIN_Vaporizer_on, HIGH);
+    delay(500);
+    digitalWrite(PIN_Vaporizer_on, LOW);
+  }
+  if (!digitalRead(PIN_Vaporizer_power) && !flag) {
+    flag = !flag;
+  }
+  unsigned long currentMillis = millis ( );
+  if ( State == LOW ) {
+    if ( currentMillis - previousMillis >= interval_1 ) {
+      previousMillis = currentMillis;
+      State = HIGH;
+      digitalWrite (ledPin, State );
+    }
+  }
+  if ( State == HIGH ) {
+    if ( currentMillis - previousMillis >= interval_2 ) {
+      previousMillis = currentMillis;
+      State = LOW;
+      digitalWrite ( ledPin, State );
+    }
+  }
+  Serial.print("Влажность: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Температура: ");
+  Serial.print(t);
+  Serial.println(" *C "); //Вывод показателей на экран
 }
