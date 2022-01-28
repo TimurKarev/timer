765c307aae1ca54f993296059fa5762bf908b378
Nik11978 <lilu_90@list.ru>
2022-01-28 22:20:14 +0300

    Последняя рабочая 28.01.22

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "DHT.h"
-#include <DHT_U.h>   // когда на чистом си собираешь еще и такое надо ставить
 #define DHTPIN 2 // Тот самый номер пина, о котором упоминалось выше
 // Одна из следующих строк закоментирована. Снимите комментарий, если подключаете датчик DHT11 к arduino
 //DHT dht(DHTPIN, DHT22); //Инициация датчика
@@ -8,20 +7,25 @@ DHT dht(DHTPIN, DHT11);
 #define PIN_Fan 6
 #define PIN_Vaporizer_power 7
 #define PIN_Vaporizer_on 8
-boolean flag = true;
+int flag = 0;
 const int ledPin = 4;
 int State = LOW;
 unsigned long previousMillis;
-const long interval_1 = 64800000; //Выкл
-const long interval_2 = 21600000; //Вкл
+const long interval_1 = 21600000; //Вкл 21600000 (6ч)
+const long interval_2 = 64800000;  //Выкл 64800000 (18ч)
 void setup() {
   Serial.begin(9600);
   dht.begin();
   pinMode(PIN_Heating, OUTPUT); // Объявляем пин реле как выход
+  digitalWrite(PIN_Heating, HIGH);
   pinMode(PIN_Fan, OUTPUT);
+  digitalWrite(PIN_Fan, HIGH);
   pinMode(PIN_Vaporizer_power, OUTPUT);
+  digitalWrite(PIN_Vaporizer_power, HIGH);
   pinMode(PIN_Vaporizer_on, OUTPUT);
+  digitalWrite(PIN_Vaporizer_on, HIGH);
   pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
 }
 void loop() {
   delay(2000); // 2 секунды задержки
@@ -31,39 +35,37 @@ void loop() {
     Serial.println("Ошибка считывания");
     return;
   }
-  {
-    if ((t) < 25) {
-      digitalWrite(PIN_Heating, HIGH); //У нас подключено так, что включается при помощи LOW
+  if ((t) < 25) {
+      digitalWrite(PIN_Heating, LOW); //У нас подключено так, что включается при помощи LOW
     }
     if ((t) > 27) {
-      digitalWrite(PIN_Heating, LOW);  // а в эмуляторе ты подключил по другому, поэтому там работает а тут нет
+      digitalWrite(PIN_Heating, HIGH);  // а в эмуляторе ты подключил по другому, поэтому там работает а тут нет
     }
-    //Вытяжка
+       //Вытяжка
     if ((t) > 29 || (h) > 95) {
-      digitalWrite(PIN_Fan, HIGH);
+      digitalWrite(PIN_Fan, LOW);
     }
     if ((t) < 29 && (h) < 90) {  // сравнение это &&, одно & это операция побитного И
-      digitalWrite(PIN_Fan, LOW);
+      digitalWrite(PIN_Fan, HIGH);
     }
     //Испаритель питание
     if ((h) < 75) {
-      digitalWrite(PIN_Vaporizer_power, HIGH);
+      digitalWrite(PIN_Vaporizer_power, LOW);
     }
     if ((h) > 80) {
-      digitalWrite(PIN_Vaporizer_power, LOW);
-      delay(500);
+      digitalWrite(PIN_Vaporizer_power, HIGH);
+      
     }
-  }
+  
   //Испаритель управление
-  if (digitalRead(PIN_Vaporizer_power) && flag) {
-    flag = !flag;
-    digitalWrite(PIN_Vaporizer_on, HIGH);
+  if (digitalRead(PIN_Vaporizer_power)== LOW && flag == 0) {
     delay(500);
     digitalWrite(PIN_Vaporizer_on, LOW);
-  }
-  if (!digitalRead(PIN_Vaporizer_power) && !flag) {
-    flag = !flag;
-  }
+    delay(500);
+    digitalWrite(PIN_Vaporizer_on, HIGH);
+    flag = 1;}
+    if (digitalRead(PIN_Vaporizer_power)== HIGH && flag == 1) {
+    flag = 0;}
   unsigned long currentMillis = millis ( );
   if ( State == LOW ) {
     if ( currentMillis - previousMillis >= interval_1 ) {
@@ -79,11 +81,11 @@ void loop() {
       digitalWrite ( ledPin, State );
     }
   }
-
   Serial.print("Влажность: ");
   Serial.print(h);
   Serial.print(" %\t");
   Serial.print("Температура: ");
   Serial.print(t);
   Serial.println(" *C "); //Вывод показателей на экран
-}
+  Serial.print("Испаритель 1-откл/0-вкл :");
+  Serial.println(digitalRead(PIN_Vaporizer_power));
